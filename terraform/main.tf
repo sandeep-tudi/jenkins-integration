31dfd5f899da8424bbc7c60526d8ad3c4bf88a79
sandeep.tudi <sandeep.tudi@qentelli.com>
2023-06-23 15:05:36 +0530

    Adding terraform folder

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = us-east-1
+  profile = default
+
+}
+
+resource "aws_instance" "terraform_instace" {
+    ami = ami-022e1a32d3f742bd8
+    instance_type = t2.micro
+}
